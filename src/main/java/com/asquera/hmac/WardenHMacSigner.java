d97cc2397e29189278e41ba20a568b77417e9edf
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-07-16 12:43:25 +0200

    removed Map Utils class and test cases, HashMap already has such a merge method (putAll)

--- src/main/java/com/asquera/hmac/WardenHMacSigner.java
+++ src/main/java/com/asquera/hmac/WardenHMacSigner.java
@@ -1,12 +1,24 @@
 package com.asquera.hmac;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
 class WardenHMacSigner {
     
     private final String algorithm;
-    private final Map<String, Object> defaultOptions;
+    private final Map<String, Object> options;
+    private static final Map<String, Object> defaultOptions = createDefaultMap();
+    
+    private static Map<String, Object> createDefaultMap() {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("auth_scheme", "HMAC");
+        map.put("auth_param", "auth");
+        map.put("auth_header", "Authorization");
+        map.put("auth_header_format", "%{auth_scheme} %{signature}");
+        //map.put("nonce_header", interpolateString());
+        return Collections.unmodifiableMap(map);
+    }
     
     public WardenHMacSigner() {
         this("sha1", new HashMap<String, Object>());
@@ -18,6 +30,7 @@ class WardenHMacSigner {
     
     public WardenHMacSigner(final String algorithm, final Map<String, Object> options) {
         this.algorithm = algorithm;
-        this.defaultOptions = options;
+        this.options = new HashMap<String, Object>(defaultOptions);
+        this.options.putAll(options);
     }
 }

658c29d13321513bdfc07e8717c8498884f4a1d5
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-07-25 15:00:29 +0200

    modified shell script, warden module gem is installed

--- scripts/install_warden_module.sh
+++ scripts/install_warden_module.sh
@@ -8,16 +8,14 @@ set -e
 gem install sinatra
 gem install bundler
 
-# checkout warden module
-git clone git://github.com/justahero/warden-hmac-authentication.git ../authentication
-# build gem and install it
-cd ../../authentication
+cd ../authentication
+
 bundle install
 gem build warden-hmac-authentication.gemspec
 gem install warden-hmac-authentication-0.6.2.gem
 
 # get back to origin directory
-#cd $currentdir
+cd $currentdir
 
 # start sinatra web app
 #cd scripts/sinatra

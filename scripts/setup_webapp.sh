c46ace181e18fb17c4a688342e9770af038c56e6
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-07-25 14:08:08 +0200

    moved sinatra web app to scripts folder, a shell script checks out warden module and installs the gem

---
+++ scripts/setup_webapp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+currentdir=`pwd`
+
+# fail on errors
+set -e
+
+# install ruby gems
+gem install sinatra
+gem install bundler
+
+# checkout warden module
+git clone git://github.com/justahero/warden-hmac-authentication.git ../authentication
+# build gem and install it
+cd ../authentication
+bundle install
+gem build warden-hmac-authentication.gemspec
+gem install warden-hmac-authentication-0.6.2.gem
+
+# get back to origin directory
+#cd $currentdir
+
+# start sinatra web app
+#cd scripts/sinatra

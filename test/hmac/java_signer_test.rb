5829f540ee50e9f9c181f6e4918dd2ac798c6244
Sebastian Ziebell <sebastian.ziebell@asquera.de>
2012-08-24 15:31:45 +0200

    Fixed test, both signers use same time stamp to sign url

--- test/hmac/java_signer_test.rb
+++ test/hmac/java_signer_test.rb
@@ -2,6 +2,7 @@ require 'java'
 
 java_import javax.crypto.Mac
 java_import com.asquera.hmac.WardenHMacSigner
+java_import com.asquera.hmac.RequestParams
 
 class SignerTest < Test::Unit::TestCase
   attr_accessor :javasigner, :hmacsigner
@@ -13,11 +14,15 @@ class SignerTest < Test::Unit::TestCase
       self.hmacsigner = HMAC::Signer.new("md5")
     end
 
-    should "matches the signed url" do
-      actual   = hmacsigner.sign_url("/test", "secret")
-      expected = javasigner.sign_url("/test", "secret")
-      
+    should "match the signed url with same time stamp" do
+      actual = hmacsigner.sign_url("/test", "secret", :date => "Fri, 24 Aug 2012 14:44:15 GMT")
+
+      options = RequestParams.new()
+      options.set_date(2012, 7, 24, 14, 44, 15)
+      expected = javasigner.sign_url("/test", "secret", options)
+
       assert_equal(actual, expected)
     end
+
   end
 end
